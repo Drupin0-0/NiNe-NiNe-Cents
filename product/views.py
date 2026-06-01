2429dde3719c02397a70f140a7c2f884957a1650
Eduardo <linseduardo2009@gmail.com>
2026-06-01 12:12:59 -0300

    add routes and views

--- product/views.py
+++ product/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from djago.http import HttpResponse
 
 # Create your views here.
+def product_reister(request):
+    if(request.method == 'POST'):
+        pass
+
+    return HttpResponse('<h1>API SEM ACESSO WEB</h1>')
